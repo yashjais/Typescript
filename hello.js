8ee75ab853a68a02121d7cd11c4261bc7b8665fd
Yash Jaiswal <jais.yash01@gmail.com>
2026-05-13 23:44:13 +0530

    Basics

--- hello.js
+++ hello.js
@@ -1,2 +1,7 @@
 "use strict";
 console.log('hello world');
+function greet(person, date) {
+    console.log(`Hello ${person}. Today is ${date}`);
+}
+;
+greet('Brendon', new Date());
--- hello.ts
+++ hello.ts
@@ -1,1 +1,7 @@
 console.log('hello world');
+
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}. Today is ${date}`);
+};
+
+greet('Brendon', new Date());
